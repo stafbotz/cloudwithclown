96700e956267660a9e4fc96f2e15f65dde659dc3
stafbotz <90103953+stafbotz@users.noreply.github.com>
2021-11-06 02:14:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ var express = require('express'),
     secure = require('ssl-express-www');
 const PORT = process.env.PORT || 8080 || 5000 || 3000
 var { color } = require('./lib/color.js');
-const fs = require('fs-extra');
-const { Client } = require ('whatsapp-web.js');
 
 var mainrouter = require('./routes/main'),
     apirouter = require('./routes/api')
@@ -22,36 +20,5 @@ app.use('/api', apirouter)
 app.listen(PORT, () => {
     console.log(color("Server running on port " + PORT,'green'))
 })
-
-
-const sessionfile = './session.json';
-let sessionconfig;
-if (fs.exitsSync(sessionfile) {
-    sessionconfig = require(sessionfile);
-}
-const client = new Client({ puppeteer: { headless: true }, session: sessionconfig });
-client.on('qr', (qr) => {
-   qrcode.generate(qr, {small: true});
-})
-client.on('authenticated', (session) => {
-    console.log('Authenticated', session);
-    sessionconfig = session;
-    fs.writeFile(sessionfile, JSON.stringify(session), function (err) {
-        if (err) {
-            console.error(err);
-        }
-    })
-})
-client.on('ready', () => {
-   console.log('Client is ready!');
-})
-client.on('message', msg => {
-   if (msg.body == '!ping') {
-       msg.reply('pong');
-   }
-})
-client.initialize();
-
-
 
 module.exports = app
