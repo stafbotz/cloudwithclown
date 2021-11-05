ff580e40b0a29ec2173c529dd2b9c6e796d5c328
stafbotz <90103953+stafbotz@users.noreply.github.com>
2021-11-06 01:45:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var express = require('express'),
 const PORT = process.env.PORT || 8080 || 5000 || 3000
 var { color } = require('./lib/color.js');;
 const fs = require('fs-extra');
+const { Client } = require ('whatsapp-web.js')
 
 var mainrouter = require('./routes/main'),
     apirouter = require('./routes/api')
@@ -22,6 +23,21 @@ app.listen(PORT, () => {
     console.log(color("Server running on port " + PORT,'green'))
 })
 
+const client = new Client();
+client.on('qr', (qr) => {
+   qrcode.generate(qr)
+})
 
+client.on('ready', () => {
+   console.log('Client is ready!')
+})
+
+client.on('message', msg => {
+   if (msg.body == '!ping') {
+       msg.reply('pong')
+   }
+})
+
+client.initialize();
 
 module.exports = app
