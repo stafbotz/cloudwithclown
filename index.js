541c47f947f3721695ceb87cec0303bffd393705
stafbotz <90103953+stafbotz@users.noreply.github.com>
2021-11-06 02:59:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var express = require('express'),
     cors = require('cors'),
     secure = require('ssl-express-www');
+
+const { WAConnection, MessageType, Presence, Mimetype, relayWAMessage, prepareMessageFromContent, GroupSettingChange } = require ('@adiwajshing/baileys');
+var { color } = require('./lib/color.js');
 const PORT = process.env.PORT || 8080 || 5000 || 3000
 const fs = require('fs-extra');
 const qrcode =  require('qrcode-terminal');
-var { color } = require('./lib/color.js');
-const { Client } = require ('whatsapp-web.js');
 
 
 var mainrouter = require('./routes/main'),
@@ -24,35 +25,5 @@ app.use('/api', apirouter)
 app.listen(PORT, () => {
     console.log(color("Server running on port " + PORT,'green'))
 })
-
-const sessionfile = './session.json';
-let sessionconfig;
-if (fs.existsSync(sessionfile)) {
-    sessionconfig = require(sessionfile);
-}
-const client = new Client({ puppeteer: { headless: true }, session: sessionconfig });
-client.on('qr', (qr) => {
-   qrcode.generate(qr, {small: true});
-})
-client.on('authenticated', (session) => {
-    console.log('Authenticated', session);
-    sessionconfig = session;
-    fs.writeFile(sessionfile, JSON.stringify(session), function (err) {
-        if (err) {
-            console.error(err);
-        }
-    })
-})
-client.on('ready', () => {
-   console.log('Client is ready!');
-})
-client.on('message', msg => {
-   if (msg.body == '!ping') {
-       msg.reply('pong');
-   }
-})
-client.initialize();
-
-
 
 module.exports = app
