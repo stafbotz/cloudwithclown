9e26e45421e78a37477f8d58aa981fd2abd33211
stafbotz <90103953+stafbotz@users.noreply.github.com>
2021-11-06 02:03:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -23,21 +23,35 @@ app.listen(PORT, () => {
     console.log(color("Server running on port " + PORT,'green'))
 })
 
-const client = new Client();
-client.on('qr', (qr) => {
-   qrcode.generate(qr)
-})
 
+const sessionfile = './session.json'
+let sessionconfig
+if (fs.exitsSync(sessionfile) {
+    sessionconfig = require(sessionfile)
+}
+const client = new Client({ puppeteer: { headless: true }, session: sessionconfig })
+client.on('qr', (qr) => {
+   qrcode.generate(qr, {small: true})
+})
+client.on('authenticated', (session) => {
+    console.log('Authenticated', session)
+    sessionconfig = session
+    fs.writeFile(sessionfile, JSON.stringify(session), function (err) {
+        if (err) {
+            console.error(err)
+        }
+    })
+})
 client.on('ready', () => {
    console.log('Client is ready!')
 })
-
 client.on('message', msg => {
    if (msg.body == '!ping') {
        msg.reply('pong')
    }
 })
-
 client.initialize();
+
+
 
 module.exports = app
