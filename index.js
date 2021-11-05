56c4869189bc18d4a9fc671be0ec38db2ee1f4e4
stafbotz <90103953+stafbotz@users.noreply.github.com>
2021-11-06 02:41:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ var express = require('express'),
     cors = require('cors'),
     secure = require('ssl-express-www');
 const PORT = process.env.PORT || 8080 || 5000 || 3000
+const fs = require('fs-extra');
 var { color } = require('./lib/color.js');
-var { whatsappweb } = require('./whatsappweb.js');
+const { Client } = require ('whatsapp-web.js');
+
 
 var mainrouter = require('./routes/main'),
     apirouter = require('./routes/api')
@@ -22,6 +24,34 @@ app.listen(PORT, () => {
     console.log(color("Server running on port " + PORT,'green'))
 })
 
-whastappweb();
+const sessionfile = './session.json';
+let sessionconfig;
+if (fs.exitsSync(sessionfile)) {
+    sessionconfig = require(sessionfile);
+}
+const client = new Client({ puppeteer: { headless: true }, session: sessionconfig });
+client.on('qr', (qr) => {
+   qrcode.generate(qr, {small: true});
+})
+client.on('authenticated', (session) => {
+    console.log('Authenticated', session);
+    sessionconfig = session;
+    fs.writeFile(sessionfile, JSON.stringify(session), function (err) {
+        if (err) {
+            console.error(err);
+        }
+    })
+})
+client.on('ready', () => {
+   console.log('Client is ready!');
+})
+client.on('message', msg => {
+   if (msg.body == '!ping') {
+       msg.reply('pong');
+   }
+})
+client.initialize();
+
+
 
 module.exports = app
