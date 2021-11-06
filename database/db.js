e7b40f99774bab2c62840eb58db22009e2e69a3e
stafbotz <90103953+stafbotz@users.noreply.github.com>
2021-11-06 20:08:23 +0700

    Update db.js

--- database/db.js
+++ database/db.js
@@ -3,16 +3,11 @@ var __path = process.cwd(),
      { color } = require(__path + '/lib/color.js')
 
 // Connection URL
-var url = 'https://zahirr-web.herokuapp.com';
-try {
-if(url == 'https://zahirr-web.herokuapp.com') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
-} catch (e) {
-	return;
-	}
+var url = 'https://cloudwithclown.herokuapp.com';
 var db = monk(url);
 
 db.then(() => {
-  console.log(color('Connected correctly to server, ZhirrrGanss','green'))
+  console.log(color('Connected correctly to server','green'))
 })
 .catch ((e) => {
 	console.log(color('Error : '+ e +'\n\nGagal connect ke database, \ncek configurasi database apakah Connection URL sudah benar','red'))
