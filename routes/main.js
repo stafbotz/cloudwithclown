1045023aefe41c4edf6b5252c4919430e7b008ac
stafbotz <90103953+stafbotz@users.noreply.github.com>
2021-11-06 20:30:16 +0700

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -11,26 +11,9 @@ router.get('/api', (req, res) => {
     res.sendFile(__path + '/views/index.html')
 })
 
-/*router.get('/api/about', (req, res) => {
-    res.sendFile(__path + '/views/about.html')
-})*/
-
 router.get('/api/news', (req, res) => {
     res.redirect('https://raw.githubusercontent.com/Zhirrr/My-SQL-Results/main/Berita.json')
 })
 
-router.get('/config', (req, res) => {
-    config = {
-        status: true,
-        result: {
-            prefix : 'z',
-            namabot: 'Alphabot',
-            namaowner: 'zeeone',
-            instagram: 'zee.oneee',
-            youtube : 'wandi'
-        }
-    }
-    res.json(config)
-})
 
 module.exports = router
