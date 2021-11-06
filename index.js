2462176120226e7c3cdcf769ccbd7961971abf49
stafbotz <90103953+stafbotz@users.noreply.github.com>
2021-11-06 15:27:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -146,7 +146,7 @@ namedir = q.split('|')[1]
 
 resultdir = './dir/' + namedir
 splitdir = namedir.split('/')[0]
-pinaccesdir = fs.open('./dir/' + splitdir + '/pinacces', 'r')
+pinaccesdir = await fs.open('./dir/' + splitdir + '/pinacces', 'r')
 
 console.log(pinaccesdir)
 if (!fs.existsSync(resultdir)) return
