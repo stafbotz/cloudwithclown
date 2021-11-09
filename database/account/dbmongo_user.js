1cf14d774652a629136381d0c44ae2714d590d92
stafbotz <90103953+stafbotz@users.noreply.github.com>
2021-11-09 22:29:38 +0700

    Update dbmongo_user.js

--- database/account/dbmongo_user.js
+++ database/account/dbmongo_user.js
@@ -1,3 +1,16 @@
-const { MongoClient } = require('mongodb')
+const { MongoClient } = require('mongodb');
 
-const uri = "mongodb+srv://cloudwithclown:ZH5Knz@-#5n6YRT@cloudwithclown.y9f8f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = "mongodb+srv://cloudwithclown:ZH5Knz@-#5n6YRT@cloudwithclown.y9f8f.mongodb.net";
+const dbName = 'account';
+   
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+client.connect((error, client) => {
+  if (error) {
+     return console.log('connection failed');
+  }
+  console.log('connection successful');
+});
