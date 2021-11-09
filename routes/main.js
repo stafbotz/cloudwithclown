40c15383fe86187f026fd22c944b5fa1a01b90dc
stafbotz <90103953+stafbotz@users.noreply.github.com>
2021-11-09 16:24:33 +0700

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -4,7 +4,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    res.redirect('/api')
+    res.sendFile(__path + 'views/home.html')
 })
 
 router.get('/api', (req, res) => {
