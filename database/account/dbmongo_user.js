800d180a9ad883466ab02785a8e07d68861fd213
stafbotz <90103953+stafbotz@users.noreply.github.com>
2021-11-09 22:48:21 +0700

    Update dbmongo_user.js

--- database/account/dbmongo_user.js
+++ database/account/dbmongo_user.js
@@ -1,5 +1,5 @@
 const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://cloudwithclown:ZH5Knz%40-%235n6YRT@cloudwithclown.y9f8f.mongodb.net";
+const uri = "mongodb+srv://cloudwithclown:ZH5Knz%40-%235n6YRT@cloudwithclown.y9f8f.mongodb.net/account?retryWrites=true&w=majority";
    
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
