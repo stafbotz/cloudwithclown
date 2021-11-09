ff7a0403494516960be0360a31155ecf2ce1ff3b
stafbotz <90103953+stafbotz@users.noreply.github.com>
2021-11-09 14:44:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -129,4 +129,14 @@ app.post('/jsondatabase/login', (req, res) => {
     }
 });
 
+app.use((req, res, next) => {
+    // Get auth token from the cookies
+    const authToken = req.cookies['AuthToken'];
+
+    // Inject the user to the request
+    req.user = authTokens[authToken];
+
+    next();
+});
+
 module.exports = app
