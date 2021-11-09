93782c91892937f0ca3e8bb7ad7c7bda760ba4be
stafbotz <90103953+stafbotz@users.noreply.github.com>
2021-11-09 21:55:26 +0700

    Update dbmongo_user.js

--- database/account/dbmongo_user.js
+++ database/account/dbmongo_user.js
@@ -1,3 +1,3 @@
 const { MongoClient } = require('mongodb')
 
-const uri = "mongodb+srv://cloudwithclown:ZH5Knz@-#5n6YRT@cluster0.x4n1f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = "mongodb+srv://cloudwithclown:ZH5Knz@-#5n6YRT@cloudwithclown.y9f8f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
