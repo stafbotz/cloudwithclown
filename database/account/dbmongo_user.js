bb51af777e1a963a4628e6569505d224dab77fa7
stafbotz <90103953+stafbotz@users.noreply.github.com>
2021-11-10 07:23:14 +0700

    Delete dbmongo_user.js

--- database/account/dbmongo_user.js
+++
@@ -1,7 +0,0 @@
-const mongoose= require('mongoose');
-
-mongoose.connect('mongodb://cloudwithclown:ZH5Knz@-#5n6YRT@cloudwithclown-shard-00-00.y9f8f.mongodb.net:27017/account?retryWrites=true&w=majority')
-.then(() => {
-    console.log('Connection succes');
-})
-.catch(err => console.log('Error: ' + err));
