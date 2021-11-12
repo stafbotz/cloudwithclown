9152d7b3306dc0a5946a132a676b521bb48fd42a
stafbotz <90103953+stafbotz@users.noreply.github.com>
2021-11-12 22:38:30 +0700

    Update dblocal.js

--- routes/dblocal.js
+++ routes/dblocal.js
@@ -1,1 +1,135 @@
+//fs.mkdir
+app.get('/jsondatabase', function (req, res) {
+    res.render('home');
+});
+app.get('/jsondatabase/register', (req, res) => {
+    res.render('register');
+});
 
+const crypto = require('crypto');
+const getHashedPassword = (password) => {
+    const sha256 = crypto.createHash('sha256');
+    const hash = sha256.update(password).digest('base64');
+    return hash;
+};
+
+const users = [
+    // This user is added to the array to avoid creating a new user on each restart
+    {
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'johndoe@email.com',
+        // This is the SHA256 hash for value of `password`
+        password: 'XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg='
+    }
+];
+
+app.post('/jsondatabase/register', (req, res) => {
+    const { email, firstName, lastName, password, confirmPassword } = req.body;
+
+    // Check if the password and confirm password fields match
+    if (password === confirmPassword) {
+
+        // Check if user with the same email is also registered
+        if (users.find(user => user.email === email)) {
+
+            res.render('register', {
+                message: 'User already registered.',
+                messageClass: 'alert-danger'
+            });
+
+            return;
+        }
+
+        const hashedPassword = getHashedPassword(password);
+
+        // Store user into the database if you are using one
+        users.push({
+            firstName,
+            lastName,
+            email,
+            password: hashedPassword
+        });
+
+        res.render('login', {
+            message: 'Registration Complete. Please login to continue.',
+            messageClass: 'alert-success'
+        });
+    } else {
+        res.render('register', {
+            message: 'Password does not match.',
+            messageClass: 'alert-danger'
+        });
+    }
+});
+
+app.get('/jsondatabase/login', (req, res) => {
+    res.render('login');
+});
+
+const generateAuthToken = () => {
+    return crypto.randomBytes(30).toString('hex');
+}
+
+// This will hold the users and authToken related to users
+const authTokens = {};
+
+app.post('/jsondatabase/login', (req, res) => {
+    const { email, password } = req.body;
+    const hashedPassword = getHashedPassword(password);
+
+    const user = users.find(u => {
+        return u.email === email && hashedPassword === u.password
+    });
+
+    if (user) {
+        const authToken = generateAuthToken();
+
+        // Store authentication token
+        authTokens[authToken] = user;
+        
+        // Setting the auth token in cookies
+        res.cookie('AuthToken', authToken);
+
+        // Redirect user to the protected page
+        res.redirect('/jsondatabase/protected');
+    } else {
+        res.render('login', {
+            message: 'Invalid username or password',
+            messageClass: 'alert-danger'
+        });
+    }
+});
+
+app.use((req, res, next) => {
+    // Get auth token from the cookies
+    const authToken = req.cookies['AuthToken'];
+  
+    // Inject the user to the request
+    req.user = authTokens[authToken];
+
+    next();
+});
+
+app.get('/jsondatabase/protected', (req, res) => {
+    if (req.user) {
+        res.render('protected');
+    } else {
+        res.render('login', {
+            message: 'Please login to continue',
+            messageClass: 'alert-danger'
+        });
+    }
+});
+
+app.get('/jsondatabase/logout', function(req, res) {
+    cookie = req.cookies;
+    for (var prop in cookie) {
+        if (!cookie.hasOwnProperty(prop)) {
+            continue;
+        }
+        res.cookie(prop, '', {expires: new Date(0)});
+        console.log("user logged out.")
+    }
+    res.redirect('/jsondatabase/login');
+});
