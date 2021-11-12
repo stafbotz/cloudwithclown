55126367f18b7f4fe12e2d8e1f03d5561d63d598
stafbotz <90103953+stafbotz@users.noreply.github.com>
2021-11-12 13:40:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ app.post('/jsondatabase/register', (req, res) => {
 });
 
 app.get('/jsondatabase/login', (req, res) => {
-if (req.cookies['AuthToken'] !== 'undefined') return console.log(req.cookies['AuthToken']);
+if (req.cookies['AuthToken'] !== undefined) return console.log(req.cookies['AuthToken']);
     res.render('login');
 });
 
