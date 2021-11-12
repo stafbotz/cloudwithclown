e2c1c8dbe77e2cedbf9da2e9e2bd49d3ef2687e0
stafbotz <90103953+stafbotz@users.noreply.github.com>
2021-11-12 22:38:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 8080 || 5000 || 3000
 const mainrouter = require('./routes/main')
 const apirouter = require('./routes/api')
-const alokrouter = require('./routes/api')
+const dblocalrouter = require('./routes/dblocal')
 
 const app = express()
 app.enable('trust proxy')
@@ -24,144 +24,9 @@ app.engine('hbs', exphbs({extname: '.hbs'}))
 app.set('view engine', 'hbs')
 app.use('/', mainrouter)
 app.use('/api', apirouter)
-app.use('/jsondatabase', dbjsonrouter)
+app.use('/jsondatabase', dblocalrouter)
 app.listen(PORT, () => {
     console.log(color("Server running on port " + PORT, 'green'))
 })
-//fs.mkdir
-app.get('/jsondatabase', function (req, res) {
-    res.render('home');
-});
-app.get('/jsondatabase/register', (req, res) => {
-    res.render('register');
-});
-
-const crypto = require('crypto');
-const getHashedPassword = (password) => {
-    const sha256 = crypto.createHash('sha256');
-    const hash = sha256.update(password).digest('base64');
-    return hash;
-};
-
-const users = [
-    // This user is added to the array to avoid creating a new user on each restart
-    {
-        firstName: 'John',
-        lastName: 'Doe',
-        email: 'johndoe@email.com',
-        // This is the SHA256 hash for value of `password`
-        password: 'XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg='
-    }
-];
-
-app.post('/jsondatabase/register', (req, res) => {
-    const { email, firstName, lastName, password, confirmPassword } = req.body;
-
-    // Check if the password and confirm password fields match
-    if (password === confirmPassword) {
-
-        // Check if user with the same email is also registered
-        if (users.find(user => user.email === email)) {
-
-            res.render('register', {
-                message: 'User already registered.',
-                messageClass: 'alert-danger'
-            });
-
-            return;
-        }
-
-        const hashedPassword = getHashedPassword(password);
-
-        // Store user into the database if you are using one
-        users.push({
-            firstName,
-            lastName,
-            email,
-            password: hashedPassword
-        });
-
-        res.render('login', {
-            message: 'Registration Complete. Please login to continue.',
-            messageClass: 'alert-success'
-        });
-    } else {
-        res.render('register', {
-            message: 'Password does not match.',
-            messageClass: 'alert-danger'
-        });
-    }
-});
-
-app.get('/jsondatabase/login', (req, res) => {
-    res.render('login');
-});
-
-const generateAuthToken = () => {
-    return crypto.randomBytes(30).toString('hex');
-}
-
-// This will hold the users and authToken related to users
-const authTokens = {};
-
-app.post('/jsondatabase/login', (req, res) => {
-    const { email, password } = req.body;
-    const hashedPassword = getHashedPassword(password);
-
-    const user = users.find(u => {
-        return u.email === email && hashedPassword === u.password
-    });
-
-    if (user) {
-        const authToken = generateAuthToken();
-
-        // Store authentication token
-        authTokens[authToken] = user;
-        
-        // Setting the auth token in cookies
-        res.cookie('AuthToken', authToken);
-
-        // Redirect user to the protected page
-        res.redirect('/jsondatabase/protected');
-    } else {
-        res.render('login', {
-            message: 'Invalid username or password',
-            messageClass: 'alert-danger'
-        });
-    }
-});
-
-app.use((req, res, next) => {
-    // Get auth token from the cookies
-    const authToken = req.cookies['AuthToken'];
-  
-    // Inject the user to the request
-    req.user = authTokens[authToken];
-
-    next();
-});
-
-app.get('/jsondatabase/protected', (req, res) => {
-    if (req.user) {
-        res.render('protected');
-    } else {
-        res.render('login', {
-            message: 'Please login to continue',
-            messageClass: 'alert-danger'
-        });
-    }
-});
-
-app.get('/jsondatabase/logout', function(req, res) {
-    cookie = req.cookies;
-    for (var prop in cookie) {
-        if (!cookie.hasOwnProperty(prop)) {
-            continue;
-        }
-        res.cookie(prop, '', {expires: new Date(0)});
-        console.log("user logged out.")
-    }
-    res.redirect('/jsondatabase/login');
-});
 
 module.exports = app
