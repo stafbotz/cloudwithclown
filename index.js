1ca54d1e97dbeb67437daaea490b846407c7ffe6
stafbotz <90103953+stafbotz@users.noreply.github.com>
2021-11-13 11:22:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -133,8 +133,7 @@ app.use((req, res, next) => {
 
 app.get('/jsondatabase/dashboard', (req, res) => {
     if (req.user) {
-        console.log(req.user);
-        allnamefile = fs.readdirSync('./database/hostdb/');
+        allnamefile = fs.readdirSync('./database/hostdb/' + req.user.email);
         res.render('dashboard', {
             result : allnamefile
         });
